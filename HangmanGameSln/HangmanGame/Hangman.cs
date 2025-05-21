0128ff23576e9ff1df95a946e31b50cdbf1304c3
ethanl267 <ethanlesch8@gmail.com>
2025-05-21 06:25:55 +0200

    created a random function to generate the next word after failing to enter the guessed word

--- HangmanGameSln/HangmanGame/Hangman.cs
+++ HangmanGameSln/HangmanGame/Hangman.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using HangmanRenderer.Renderer;
 
 namespace Hangman.Core.Game
@@ -6,82 +7,101 @@ namespace Hangman.Core.Game
     public class HangmanGame
     {
         private GallowsRenderer _renderer;
-        private string SecretWord = "Occupation";
+        private readonly string[] WordList = new string[]
+{
+    "Occupation", "Computer", "Programming", "Science", "Adventure",
+    "Mountain", "Library", "Elephant", "Sunshine", "Chocolate",
+    "Rainbow", "Journey", "Mystery", "Festival", "Harmony",
+    "Galaxy", "Treasure", "Courage", "Whisper", "Victory"
+};
+        private string SecretWord;
         private char[] lettersOfWord;
         private bool[] guessLetters;
         private int attempts = 6;
         private bool wordComplete = false;
         private char[] correctGuesses;
-
+        Random random = new Random();
 
         public HangmanGame()
         {
             _renderer = new GallowsRenderer();
         }
 
-        public void Run()
-        {
 
+        private void ResetGame()
+        {
             // Guess the letter of the word.
+            SecretWord = WordList[random.Next(WordList.Length)];
             lettersOfWord = SecretWord.ToUpper().ToCharArray();
             guessLetters = new bool[lettersOfWord.Length];
             correctGuesses = new char[lettersOfWord.Length];
             for (int i = 0; i < correctGuesses.Length; i++)
                 correctGuesses[i] = '-';
+            attempts = 6;
+            wordComplete = false;
+        }
 
-            while (attempts > 0 && !wordComplete)
+
+        public void Run()
+        {
+
+            while (true)
             {
+                ResetGame();
+                while (attempts > 0 && !wordComplete)
+                {
+                    _renderer.Render(5, 5, attempts);
+                    Console.SetCursorPosition(0, 13);
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("Your current guess: ");
+                    Console.WriteLine(new string(correctGuesses));
+                    Console.SetCursorPosition(0, 15);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("What is your next guess: ");
+                    var input = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(input) || input.Length != 1)
+                        continue;
+
+                    char guess = char.ToUpper(input[0]);
+                    bool found = false;
+
+                    for (int i = 0; i < lettersOfWord.Length; i++)
+                    {
+                        if (lettersOfWord[i] == guess && !guessLetters[i])
+                        {
+                            guessLetters[i] = true;
+                            correctGuesses[i] = guess;
+                            found = true;
+                        }
+                    }
+
+                    if (!found)
+                        attempts--;
+
+                    wordComplete = Array.TrueForAll(guessLetters, b => b);
+
+
+                }
+
+                if (wordComplete)
+                {
+                    Console.WriteLine("Congratulations! You guessed the word!");
+                }
+                else if (attempts == 0)
+                {
+                    Console.WriteLine($"Game Over! The word was: {SecretWord}");
+                }
+
+
                 _renderer.Render(5, 5, attempts);
                 Console.SetCursorPosition(0, 13);
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.Write("Your current guess: ");
                 Console.WriteLine(new string(correctGuesses));
-                Console.SetCursorPosition(0, 15);
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("What is your next guess: ");
-                var input = Console.ReadLine();
-
-                if (string.IsNullOrEmpty(input) || input.Length != 1)
-                    continue;
-
-                char guess = char.ToUpper(input[0]);
-                bool found = false;
-
-                for (int i = 0; i < lettersOfWord.Length; i++)
-                {
-                    if (lettersOfWord[i] == guess && !guessLetters[i])
-                    {
-                        guessLetters[i] = true;
-                        correctGuesses[i] = guess;
-                        found = true;
-                    }
-                }
-
-                if (!found)
-                    attempts--;
-
-                wordComplete = Array.TrueForAll(guessLetters, b => b);
-
-  
             }
 
-
-            if (wordComplete)
-            {
-                Console.WriteLine("Congratulations! You guessed the word!");
-            }
-            else if (attempts == 0)
-            {
-                Console.WriteLine($"Game Over! The word was: {SecretWord}");
-            }
-
-
-            _renderer.Render(5, 5, attempts);
-            Console.SetCursorPosition(0, 13);
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write("Your current guess: ");
-            Console.WriteLine(new string(correctGuesses));
 
         }
 
--- HangmanGameSln/HangmanGame/obj/Debug/net9.0/HangmanGameConsole.AssemblyInfo.cs
+++ HangmanGameSln/HangmanGame/obj/Debug/net9.0/HangmanGameConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HangmanGameConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eeb883b52048f5d9a03861acfcb954107ca0004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+233c70dfeae132317148c1e551fbe4f804022b67")]
 [assembly: System.Reflection.AssemblyProductAttribute("HangmanGameConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HangmanGameConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
